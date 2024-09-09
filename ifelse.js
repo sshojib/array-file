e42591fefc03f9519e0fab2f89c61cc4c82f4a58
sshojib <sashojib57@gmail.com>
2024-09-09 18:39:03 +0600

    my forth commit

---
+++ ifelse.js
@@ -0,0 +1,14 @@
+const name = ['shojib', 'sumi'];
+console.log(name);
+
+
+if(name.includes('shojib')){
+    console.log('he my husbend')
+}
+else{
+    console.log('he is not my husbend')
+} 
+
+
+console.log(Array.isArray(name));
+console.log(name.indicating);
