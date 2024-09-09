077c6523a64d32f02e890c27b5bfb2f92fb780d4
sshojib <sashojib57@gmail.com>
2024-09-09 18:26:52 +0600

    my third commit

---
+++ includes.js
@@ -0,0 +1,14 @@
+const books = ['The Book Thief', 'The Graveyard Book', 'The Notebook '];
+console.log(books);
+[ 'The Book Thief', 'The Graveyard Book', 'The Notebook ' ]
+
+
+console.log(books.includes('The Graveyard Book'));
+[ 'The Book Thief', 'The Graveyard Book', 'The Notebook ' ]
+true
+
+
+console.log(Array.isArray(books));
+[ 'The Book Thief', 'The Graveyard Book', 'The Notebook ' ]
+true
+true
