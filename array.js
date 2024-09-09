46888b7bd8a0c8980813c032b2eddf6785ad645a
sshojib <sashojib57@gmail.com>
2024-09-09 17:57:32 +0600

    my first commit

---
+++ array.js
@@ -0,0 +1,11 @@
+const fruits = ['apple', 'orange', 'banana', 'cherry', 'guava'];
+console.log(fruits[3]);
+fruit = 'cherry';
+
+fruits[2] = 'jambura';
+console.log(fruits);
+
+{
+cherry
+[ 'apple', 'orange', 'jambura', 'cherry', 'guava' ]
+}
